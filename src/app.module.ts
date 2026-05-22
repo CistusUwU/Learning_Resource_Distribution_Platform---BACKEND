be8fee413493a218dce60a011eac411d82572e08
CistusUwU <phammynganst@gmail.com>
2026-05-22 17:05:55 +0700

    LVTN-6: Rewite module Categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,15 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD, Reflector } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/guard/jwt-auth.guard';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     AuthModule, 
-    
+    CategoriesModule,
+
   ],
   controllers: [AppController],
   providers: [
---
+++ src/categories/categories.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { book_approval_status } from "@prisma/client";
+import { PrismaService } from "../prisma/prisma.service";
+
+@Injectable()
+export class CategoriesService {
+    constructor (private prisma: PrismaService) {}
+
+    async findAll() {
+        const majors = await this.prisma.major.findMany({
+            select: {
+                major_id: true,
+                major_name: true,
+                major_code: true,
+                _count: {
+                    select: {
+                        book_major:{
+                            where: {
+                                book: {
+                                    approval_status: book_approval_status.APPROVED,
+                                }
+                            },
+                        }
+                    }
+                },
+            },
+            orderBy: {
+                major_name: 'asc',
+            },
+        });
+
+        return majors;
+    }
+}
