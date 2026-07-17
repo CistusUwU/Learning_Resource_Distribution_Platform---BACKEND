dd3c00e121f32b518901dc0d62f1dbb72ed22608
CistusUwU <phammynganst@gmail.com>
2026-07-17 15:45:17 +0700

    tasks/PMN/LVTN-76/Rebuild-Student-Store-Page

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Get, Param, ParseIntPipe, Query } from "@nestjs/common";
 import { BookQueryDto } from "./dto/book-query.dto";
 import { BooksService } from "./books.service";
+import { CurrentUser } from "src/common/decorators/current-user.decorator";
 @Controller('books')
 export class BooksController {
     constructor (private readonly booksService: BooksService) {}
 
     @Get()
-    findAll(@Query() query: BookQueryDto){
-        return this.booksService.findAll(query);
+    findAll(@Query() query: BookQueryDto, @CurrentUser() user: { id: number }) {
+        return this.booksService.findAll(query, user.id);
     }
 
     @Get(':id')
-    findOne(@Param('id', ParseIntPipe) id: number) {
-        return this.booksService.findOne(id);
+    findOne(@Param('id', ParseIntPipe) id: number, @CurrentUser() user: { id: number }) {
+        return this.booksService.findOne(id, user.id);
     }
 }
